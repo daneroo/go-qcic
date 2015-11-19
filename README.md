1eebab01a7c4ede841cc52e1620d4da478b6d3e3
Daniel Lauzon <daniel.lauzon@gmail.com>
2015-11-19 15:56:39 -0500

    initial import

---
+++ README.md
@@ -0,0 +1,14 @@
+# go-qcic
+
+*Quis custodiet ipsos custodes*
+My own monitoring system for deployed apps.
+
+Just small script to observe that deployed systems are functioning correctly.
+
+- im-weight backup
+- gmail backup scripts
+- scrobblecast/pocketscrape (matching signatures)
+
+## Running
+
+    go run qcicstat/qcic.go
---
+++ qcicstat/qcic.go
@@ -0,0 +1,47 @@
+package main
+
+import "github.com/segmentio/go-loggly-search"
+import . "github.com/bitly/go-simplejson"
+import "log"
+import "fmt"
+import "os"
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func env(name string) string {
+	val := os.Getenv(name)
+
+	if val == "" {
+		log.Fatalf("env variable %s required for example", name)
+	}
+
+	return val
+}
+
+func main() {
+	c := search.New(env("ACCOUNT"), env("USER"), env("PASS"))
+
+	res, err := c.Query(`signature`).Size(50).From("-5h").Fetch()
+	check(err)
+
+	for _, event := range res.Events {
+		Output(event)
+	}
+
+	fmt.Println()
+}
+
+func Output(event interface{}) {
+	msg := event.(map[string]interface{})["logmsg"].(string)
+	obj, err := NewJson([]byte(msg))
+	check(err)
+
+	fmt.Println()
+	for k, v := range obj.MustMap() {
+		fmt.Printf("  %14s: %s\n", k, v)
+	}
+}
